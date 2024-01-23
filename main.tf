885e5e9764f5370af4c9242acc3ef2918f4577f9
Jayllan Abecia <abeciaj23@gmail.com>
2024-01-23 18:32:15 +1100

    init

---
+++ main.tf
@@ -0,0 +1,22 @@
+resource "azurerm_postgresql_flexible_server" "flex_server" {
+  name                   = "${local.name}-serv-${var.instance_number}"
+  resource_group_name    = avar.rg_name
+  location               = var.rg_location
+  version                = var.version
+  administrator_login    = var.administrator_login
+  administrator_password = var.administrator_password
+  storage_mb             = var.storage_mb
+  sku_name               = var.sku_name
+}
+
+resource "azurerm_postgresql_flexible_server_database" "flex_db" {
+  name      = "${local.name}-db-${var.instance_number}"
+  server_id = azurerm_postgresql_flexible_server.flex_server.id
+  collation = var.collation
+  charset   = var.charser
+
+  # prevent the possibility of accidental data loss
+  # lifecycle {
+  #   prevent_destroy = true
+  # }
+}
---
+++ variables.tf
@@ -0,0 +1,62 @@
+variable "rg_name" {
+  type        = string
+  description = "Existing Resource Group Name"
+}
+
+variable "rg_location" {
+  type        = string
+  description = "Resource Group Location"
+}
+
+variable "project_name" {
+  type        = string
+  description = "Resource Name"
+}
+
+variable "instance_number" {
+  type        = string
+  description = "Resource number"
+  default     = "01"
+}
+
+variable "administrator_login" {
+  type        = string
+  description = "Admin Username"
+  default     = "adminuser"
+}
+
+variable "administrator_password" {
+  type        = string
+  description = "Admin Password"
+  default     = "Default_P@ssw0rd123"
+}
+
+variable "version" {
+  type        = string
+  description = "DB Version"
+  default     = "12"
+}
+
+variable "storage_mb" {
+  type        = number
+  description = "Storage Size"
+  default     = 32768
+}
+
+variable "sku_name" {
+  type        = string
+  description = "Sku Name"
+  default     = "Standard_B1ms"
+}
+
+variable "collation" {
+  type        = string
+  description = "Collation"
+  default     = "en_US.utf8"
+}
+
+variable "charser" {
+  type        = string
+  description = "Charset"
+  default     = "utf8"
+}
